719ce30e3ead500c21403d2c408a914fae9005d3
Deborah Parent <deborah.morchipont@hotmail.fr>
2023-11-20 12:59:14 +0100

    exo2

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def ask
+  end
+
+  def answer
+    answer1 = 'Great'
+    answer2 = 'Silly question, get dressed and go to work!'
+    answer3 = "I don't care, get dressed and go to work!"
+
+    @answer = if params[:question] == 'I am going to work'
+      answer1
+    elsif params[:question][-1] == '?'
+      answer2
+    else
+      answer3
+    end
+  end
+end
